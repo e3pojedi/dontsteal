bdc0153112c970395e511802f845096cc1a29614
e3pojedi <e3pojedi@gmail.com>
2020-03-16 04:54:08 +1100

    4 files modified

--- src/app/addlicense/addlicense.component.ts
+++ src/app/addlicense/addlicense.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './addlicense.component.html',
   styleUrls: ['./addlicense.component.css']
 })
+
+
+
 export class AddlicenseComponent implements OnInit {
+
 
   constructor() { }
 
   ngOnInit() {
   }
+  foods: Food[] = [
+    {value: 'L', viewValue: 'Learners'},
+    {value: 'P1', viewValue: 'Provisional P1'},
+    {value: 'P2', viewValue: 'Provisional P2'},
+    {value: 'C', viewValue: 'Full License'}
+  ];
 
 }
